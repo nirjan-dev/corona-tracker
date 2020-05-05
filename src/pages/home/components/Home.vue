81f43eb8688cda2e85b3064e68e10b78c04f5925
Nirjan Khadka <nk13@pop-os.localdomain>
2020-05-05 07:41:48 +0545

    add flag icons

--- src/pages/home/components/Home.vue
+++ src/pages/home/components/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <h1>Home</h1>
+  <div class="container q-px-md">
+    <div class="row q-my-md justify-between items-center">
+      <h2 class="text-h3 title">Latest Info</h2>
+      <q-chip outline color="primary">
+        <q-avatar>
+        <!-- TODO: use local icon set for flags  -->
+          <span class="flag flag-np q-ml-sm"></span>
+        </q-avatar>
+        NP
+      </q-chip>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from '@vue/composition-api';
 export default defineComponent({
-    name: 'Home',
-
-})
+  name: 'Home'
+});
 </script>
 
-<style>
+<style scoped lang="scss"> 
+  .title {
+    margin: 0;
+  }
 
+  .flag {
+    border-radius: 50%;
+    padding: 0.2em;
+  }
 </style>
